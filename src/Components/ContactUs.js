5bb7938a55d40f401a580e0e6f366c9d674a18ca
Amulya2823 <amulyareddy58@gmail.com>
2024-05-13 16:20:19 +0530

    wrote test cases for contact us page

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,9 +1,17 @@
 const ContactUs = () => {
     return (
         <div>
-            <h1>Contact Us page</h1>
+            <h1 className="font-bold text-3xl p-4 m-2" >Contact Us page</h1>
+            <form>
+                <input className="p-4 m-2 w-60 font-semibold border-2 border-black"
+                type="text" placeholder="Enter Your Name"></input>
+                <input className="p-4 m-2 w-60 font-semibold border-2 border-black"
+                type="text" placeholder="Enter Your Message"></input>
+                <button className="rounded-lg bg-black text-white w-40 text-2xl px-4 m-4 h-14"
+                >Submit</button>
+            </form>
+
         </div>
     )
 }
-
-export default ContactUs
+export default ContactUs;
---
+++ src/Components/__tests__/contact.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import ContactUs from "../ContactUs";
+import "@testing-library/jest-dom";
+
+describe("all the test cases of contact us page", () => {
+  it("should load contact us component", () => {
+    render(<ContactUs />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should load the button component", () => {
+    render(<ContactUs />);
+    //const button = screen.getByRole("button")
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should be the correct PlaceHolder", () => {
+    render(<ContactUs />);
+    const text = screen.getByPlaceholderText("Enter Your Name");
+    expect(text).toBeInTheDocument();
+  });
+
+  test("Should load the inputBoxes", () => {
+    render(<ContactUs />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+  });
+});
